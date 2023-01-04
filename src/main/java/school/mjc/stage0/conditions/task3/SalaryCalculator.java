98fbf0f0f2c34abd7e21829039a42ab6792cbda9
Iryna_Nikalayeva <irinnik92@gmail.com>
2023-01-05 00:00:56 +0100

    Performed SalaryCalculator task

--- src/main/java/school/mjc/stage0/conditions/task3/SalaryCalculator.java
+++ src/main/java/school/mjc/stage0/conditions/task3/SalaryCalculator.java
@@ -2,5 +2,19 @@ package school.mjc.stage0.conditions.task3;
 
 public class SalaryCalculator {
     public void calculateSalary(int salary) {
+        double newSalary;
+        if (salary >= 0) {
+            if (salary <= 10000) {
+                newSalary = (salary - salary * 0.15);
+            } else if (salary <= 20000) {
+                newSalary = (salary - salary * 0.18);
+            } else {
+                newSalary = (salary - salary * 0.2);
+            }
+            System.out.println(newSalary);
+        } else {
+            System.out.println("wrong input!");
+        }
+
     }
 }
