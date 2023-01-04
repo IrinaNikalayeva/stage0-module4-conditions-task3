085816e1a9f992597939ac69f90fbb1070a63e3a
Iryna_Nikalayeva <irinnik92@gmail.com>
2023-01-05 00:19:45 +0100

    Performed AlphabeticCharacters task

--- src/main/java/school/mjc/stage0/conditions/task3/AlphabeticCharacters.java
+++ src/main/java/school/mjc/stage0/conditions/task3/AlphabeticCharacters.java
@@ -1,6 +1,18 @@
 package school.mjc.stage0.conditions.task3;
 
 public class AlphabeticCharacters {
+    String vowelSymbols = "aeiouAEIOU";
     public void vowelDeterminer(char character) {
+        if ((character >= 65 && character <= 90) || (character >= 97 && character <= 122)) {
+            if (vowelSymbols.indexOf(character) >= 0) {
+                System.out.println("Vowel");
+            }
+            else {
+                System.out.println("Consonant");
+            }
+        }
+        else {
+            System.out.println("wrong alphabet!");
+        }
     }
 }
